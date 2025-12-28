7de4544e06856a4e5bcc87c33eaad1a31d2874a8
mhrjnrahul <maharjanrahul58@gmail.com>
2025-12-28 11:39:55 +0545

    cities section

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const City = () => {
+const City = ({ image, name }) => {
   return (
-    <div>City</div>
+    <div className='relative w-full h-[35vh] overflow-hidden'>
+      <img src={image} alt={name} className="w-full h-full object-cover" />
+      
+      <div className='absolute inset-0 flex items-center justify-center'>
+        <span className='text-white text-4xl font-(--font-ui) tracking-tight drop-shadow-lg'>{name}</span>
+      </div>
+    </div>
   )
 }
 
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -7,7 +7,17 @@ export default function MainLayout() {
     <div className="min-h-screen font-(font-ui) bg--color-bg text-color-text)">
       <Navbar />
       <Outlet />
-      <City />
+      
+      <div className="cities">
+        <City image="/cities/mondstadt.png" name="Mondstadt" />
+        <City image="/cities/liyue.png" name="Liyue" />
+        <City image="/cities/inazuma.png" name="Inazuma" />
+        <City image="/cities/sumeru.png" name="Sumeru" />
+        <City image="/cities/fontaine.png" name="Fontaine" />
+        <City image="/cities/natlan.png" name="Natlan" />
+        <City image="/cities/nodkrai.png" name="Nod-Krai" />
+        <City image="/cities/coming.png" name="Coming Soon"/>
+      </div>
     </div>
   );
 }
