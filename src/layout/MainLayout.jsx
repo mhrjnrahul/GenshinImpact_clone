0d85ea40647a7e63b5d64e73a4a9c84913f1629d
mhrjnrahul <maharjanrahul58@gmail.com>
2025-12-28 15:40:05 +0545

    changes to home pages

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,26 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import City from "../components/City";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";;
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen font-(font-ui) bg--color-bg text-color-text)">
       <Navbar />
       <Outlet />
-      
-      <div className="cities">
-        <City image="/cities/mondstadt.png" name="Mondstadt" />
-        <City image="/cities/liyue.png" name="Liyue" />
-        <City image="/cities/inazuma.png" name="Inazuma" />
-        <City image="/cities/sumeru.png" name="Sumeru" />
-        <City image="/cities/fontaine.png" name="Fontaine" />
-        <City image="/cities/natlan.png" name="Natlan" />
-        <City image="/cities/nodkrai.png" name="Nod-Krai" />
-        <City image="/cities/coming.png" name="Coming Soon"/>
-      </div>
 
-      <Footer />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import City from "../components/City";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -10,6 +12,19 @@ const Home = () => {
         muted
         className="w-full h-full object-cover"
       ></video>
+
+      <div className="cities">
+        <City image="/cities/mondstadt.png" name="Mondstadt" />
+        <City image="/cities/liyue.png" name="Liyue" />
+        <City image="/cities/inazuma.png" name="Inazuma" />
+        <City image="/cities/sumeru.png" name="Sumeru" />
+        <City image="/cities/fontaine.png" name="Fontaine" />
+        <City image="/cities/natlan.png" name="Natlan" />
+        <City image="/cities/nodkrai.png" name="Nod-Krai" />
+        <City image="/cities/coming.png" name="Coming Soon"/>
+      </div>
+
+      <Footer />
     </div>
   );
 };
