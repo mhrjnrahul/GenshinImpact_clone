30bbcfaf06df8ed2bee625003b0724a6cacf6abe
mhrjnrahul <maharjanrahul58@gmail.com>
2025-12-30 11:58:58 +0545

    news section

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const NewsCard = ( { image, description } ) => {
+const NewsCard = ( { image, description, date, extra } ) => {
   return (
-    <div className='bg-black px-5 py-5 rounded-xl'>
-        <img src={image} alt={description} className="w-100 h-36 object-cover" />
-        <p className="text-white mt-2 w-70">{description}</p>
+    <div className='bg-black/60 px-5 py-5 rounded flex flex-col h-full'>
+        <img src={image} alt={description} className="w-100 h-48 object-cover" />
+        <p className="text-white mt-2 w-100">{description}</p>
+
+        <div className='w-full flex justify-between mt-auto pt-4'>
+          <span className="text-white">{date}</span>
+          <span className='text-white'>{extra}</span>
+        </div>
     </div>
   )
 }
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -6,23 +6,29 @@ const News = () => {
     {
      image: "news/news1.png",
      description: "The web event Mili Snow Resort, Party in Wonderland!",
+     date: "Dec 30, 2025",
+     extra: "Events"
     },
     {
       image: "news/news2.png",
-      description: "Event Coming Soon!",
+      description: "Cutscene Animation: Where the Moon Belongs | Genshin Impact",
+      date: "Dec 21, 2025",
+      extra: "Info"
     },
     {
       image: "news/news3.png",
-      description: "New Weapon Released!",
+      description: "New Event: New Year's Advice from Teyvat",
+      date: "Dec 12, 2025",
+      extra: "Info"
     }
   ];
 
   return (
     <div className="w-full h-screen" style={{backgroundImage: 'url(/news/news_bg.png)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
       <div className="news-grid w-full h-full flex items-center justify-center">
-        <div className="news-items flex gap-6">
+        <div className="news-items grid grid-cols-3 gap-6 items-stretch mx-25">
           {news.map((item, index) => (
-            <NewsCard key={index} image={item.image} description={item.description} />
+            <NewsCard key={index} image={item.image} description={item.description} date={item.date} extra={item.extra} />
           ))}
         </div>
       </div>
