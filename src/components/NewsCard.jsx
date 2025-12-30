20918c44d696f9493a18d3de04b594d7e7bd6135
mhrjnrahul <maharjanrahul58@gmail.com>
2025-12-30 12:31:40 +0545

    changes

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const NewsCard = ( { image, description, date, extra } ) => {
   return (
     <div className='bg-black/60 px-5 py-5 rounded flex flex-col h-full'>
-        <img src={image} alt={description} className="w-100 h-48 object-cover" />
+        {/* <img src={image} alt={description} className="w-100 h-48 object-cover" />
         <p className="text-white mt-2 w-100">{description}</p>
 
         <div className='w-full flex justify-between mt-auto pt-4'>
           <span className="text-white">{date}</span>
           <span className='text-white'>{extra}</span>
-        </div>
+        </div> */}
     </div>
   )
 }
