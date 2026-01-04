dc0cecfbd1566afcfd2ed31fc3398b12cd49b0be
mhrjnrahul <maharjanrahul58@gmail.com>
2026-01-04 12:06:21 +0545

    updates

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const City = ({ image, name }) => {
+const City = ({ image, name, icon }) => {
   return (
-    <div className='relative w-full h-[35vh] overflow-hidden'>
-      <img src={image} alt={name} className="w-full h-full object-cover" />
+    <div className="group relative w-full h-[35vh] overflow-hidden cursor-pointer">
+      <img
+        src={image}
+        alt={name}
+        className="w-full h-full object-cover transition-transform duration-500 ease-out group-hover:border-4 group-hover:scale-110"
+      />
+
+      {/* White border overlay */}
+      <div className="absolute inset-0 transition-all duration-500 ease-out group-hover:border-4 group-hover:border-white pointer-events-none"></div>
       
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <span className='text-white text-4xl font-(--font-ui) tracking-tight drop-shadow-lg'>{name}</span>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="relative flex items-center justify-center w-full">
+          {/* City name - always centered */}
+          <span className="text-white text-4xl font-(--font-ui) tracking-tight drop-shadow-lg group-hover:underline decoration-2 underline-offset-4 transition-all duration-300">
+            {name}
+          </span>
+          
+          {/* Icon positioned 50px to the right of the name */}
+          {icon && (
+            <div className="absolute left-[calc(50%+300px)] translate-x-[-50%] flex items-center justify-center">
+              <img
+                src={icon}
+                alt=""
+                className="w-full h-full object-cover opacity-0 transform -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300"
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default City
+export default City;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ import Footer from "../components/Footer";
 
 const Home = () => {
   const cities = [
-  { image: "/cities/mondstadt.png", name: "Mondstadt" },
-  { image: "/cities/liyue.png", name: "Liyue" },
-  { image: "/cities/inazuma.png", name: "Inazuma" },
-  { image: "/cities/sumeru.png", name: "Sumeru" },
-  { image: "/cities/fontaine.png", name: "Fontaine" },
-  { image: "/cities/natlan.png", name: "Natlan" },
-  { image: "/cities/nodkrai.png", name: "Nod-Krai" },
+  { image: "/cities/mondstadt.png", name: "Mondstadt", icon:"/cities/jean.png" },
+  { image: "/cities/liyue.png", name: "Liyue", icon: "/cities/xiao.png" },
+  { image: "/cities/inazuma.png", name: "Inazuma", icon:"/cities/ayaka.png" },
+  { image: "/cities/sumeru.png", name: "Sumeru", icon:"/cities/tighnari.png" },
+  { image: "/cities/fontaine.png", name: "Fontaine", icon:"/cities/furina.png" },
+  { image: "/cities/natlan.png", name: "Natlan", icon:"cities/mualani.png" },
+  { image: "/cities/nodkrai.png", name: "Nod-Krai", icon:"cities/lauma.png" },
   { image: "/cities/coming.png", name: "Coming Soon" },
 ];
 
@@ -27,7 +27,7 @@ const Home = () => {
 
       <div className="cities">
         {cities.map((city, index) => (
-          <City key={index} image={city.image} name={city.name} />
+          <City key={index} image={city.image} name={city.name} icon={city.icon}/>
         ))}
       </div>
 
